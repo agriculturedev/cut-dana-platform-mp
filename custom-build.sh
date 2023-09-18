6b9ebe07a28ec777f2668fe11b265af467fe3244
Jonas Faber <jonas.h.faber@gmail.com>
2023-09-18 14:04:52 +0200

    small updates

--- custom-build.sh
+++ custom-build.sh
@@ -9,14 +9,25 @@ echo "Building the npm app..."
 npm run buildPortal
 
 # Step 2: Go to the build directory
-cd build
+cd dist
 
-# Step 3: Copy the Dockerfile to the build directory
-echo "Copying Dockerfile to build directory..."
-cp ../Dockerfile .
+# Step 3: Declare an array of app names for which you want to build Docker images
+apps=("mp-guben")
 
-# Step 4: Build the Docker image
-echo "Building Docker image..."
-docker build -t your-image-name .
+# Step 4: Copy master code to each app directory
+echo "Copying master code to app directories..."
+for app in "${apps[@]}"
+do
+    # Create the folder if it doesn't exist
+    mkdir -p "$app"
+    cp -r mastercode/* "$app/"
+done
 
-echo "Build process completed."
+# Step 5: Loop through apps and build Docker images
+for app in "${apps[@]}"
+do
+    echo "Building Docker image for $app..."
+    docker build -t "$app-image" -f ../elie/docker/Dockerfile "$app/"
+done
+
+echo "All builds are completed."
