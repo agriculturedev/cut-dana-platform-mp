7d6723b1e6badd42c94a62a41d743972dc9969e9
Mikołaj Klarżuk <klarzuk@grit.de>
2024-04-29 22:23:24 +0900

    Added prepareMultiPolygonCoords test case

--- src/modules/tools/wfst/tests/unit/utils/prepareMultiPolygonCoords.spec.js
+++ src/modules/tools/wfst/tests/unit/utils/prepareMultiPolygonCoords.spec.js
@@ -1,50 +1,82 @@
 import {expect} from "chai";
-import Feature from "ol/Feature";
-import MultiPolygon from "ol/geom/MultiPolygon.js";
-const feature1 = new Feature({
-        ol_uid: "1146",
-        geom: new MultiPolygon([
-            [
-                [567644.0615441666, 5933356.457430205],
-                [567644.0615441666, 5931229.982405349],
-                [571422.3095039128, 5933308.849780396],
-                [567644.0615441666, 5933356.457430205]
-            ]
-        ])
-    }),
-    feature2 = new Feature({
-        ol_uid: "1222",
-        geom: new MultiPolygon([
-            [
-                [568961.6858326496, 5934959.248307151],
-                [571485.8094696228, 5934610.125541875],
-                [571200.0596239277, 5937498.322963697],
-                [568961.6858326496, 5934959.248307151]
-            ]
-        ])
-    }),
-    exampleMultiPolygonFeatures = [feature1, feature2],
-    multiPolygonCoordinates = [
+import prepareMultiPolygonCoords from "../../../utils/prepareMultiPolygonCoords";
+
+const exampleMultiPolygonFeatures = [
+        {
+            ol_uid: "187",
+            getGeometry: () => ({
+                getCoordinates: () => [[[
+                    [568596.561029817, 5935514.67088827],
+                    [568469.561098397, 5932198.004618157],
+                    [571787.4343067454, 5932229.743051363],
+                    [568596.561029817, 5935514.67088827]
+                ]]],
+                intersectsCoordinate: () => false
+            })
+        },
+        {
+            ol_uid: "402",
+            getGeometry: () => ({
+                getCoordinates: () => [[[
+                    [569168.0607212072, 5933800.795495101],
+                    [569136.3107383522, 5932801.034849086],
+                    [569961.8102925824, 5932959.72701512],
+                    [569168.0607212072, 5933800.795495101]
+                ]]],
+                intersectsCoordinate: () => true
+            })
+        },
+        {
+            ol_uid: "323",
+            getGeometry: () => ({
+                getCoordinates: () => [[[
+                    [567596.4365698842, 5932198.004618157],
+                    [567596.4365698842, 5929151.115030301],
+                    [569501.4355411849, 5929278.0687631285],
+                    [567596.4365698842, 5932198.004618157]
+                ]]],
+                intersectsCoordinate: () => false
+            })
+        }
+    ],
+    exampleMultiPolygonCoordinates = [
         [
-            [567644.0615441666, 5933356.457430205],
-            [567644.0615441666, 5931229.982405349],
-            [571422.3095039128, 5933308.849780396],
-            [567644.0615441666, 5933356.457430205]
+            [
+                [568596.561029817, 5935514.67088827],
+                [568469.561098397, 5932198.004618157],
+                [571787.4343067454, 5932229.743051363],
+                [568596.561029817, 5935514.67088827]
+            ],
+            [
+                [567596.4365698842, 5932198.004618157],
+                [567596.4365698842, 5929151.115030301],
+                [569501.4355411849, 5929278.0687631285],
+                [567596.4365698842, 5932198.004618157]
+            ]
         ],
         [
-            [568961.6858326496, 5934959.248307151],
-            [571485.8094696228, 5934610.125541875],
-            [571200.0596239277, 5937498.322963697],
-            [568961.6858326496, 5934959.248307151]
+            [
+                [567596.4365698842, 5932198.004618157],
+                [567596.4365698842, 5929151.115030301],
+                [569501.4355411849, 5929278.0687631285],
+                [567596.4365698842, 5932198.004618157]
+            ]
         ]
     ];
 
 describe("src/modules/tools/wfst/utils/prepareMultiPolygonCoords.js", () => {
     it("should receive an array with at least two elements", () => {
         expect(Array.isArray(exampleMultiPolygonFeatures)).to.be.true;
-        expect(Array.length).to.be.above(1);
+        expect(exampleMultiPolygonFeatures.length).to.be.above(1);
     });
+
+    it("should create nested array of coordinates of MultiPolygons and their holes", () => {
+        const result = prepareMultiPolygonCoords(exampleMultiPolygonFeatures);
+
+        expect(result).to.deep.equal(exampleMultiPolygonCoordinates);
+    });
+
     it("should send an array with coordinates", () => {
-        expect(Array.isArray(multiPolygonCoordinates)).to.be.true;
+        expect(Array.isArray(exampleMultiPolygonCoordinates)).to.be.true;
     });
 });
